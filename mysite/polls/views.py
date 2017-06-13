1e707f2e2e4625afa19cfde0c0c41c70c5e08326
Dingchao Zhang <dingchaozhang@g.harvard.edu>
2017-06-12 20:04:28 -0500

    rewrote views of detail, index and results with class based method

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse,HttpResponseRedirect
 from django.http import Http404
 from django.urls import reverse
-from .models import Question
+from django.views import generic
+from .models import Choice, Question
 
 # Create your views here.
+"""
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
 	context = {'latest_question_list2': latest_question_list}
@@ -20,6 +22,21 @@ def results(request, question_id):
 	return render(request,'polls/results.html',{'question':question})	
 	#response = "You are looking at the results of question %s."
 	#return HttpResponse(response % question_id)
+"""
+
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list2'
+	def get_queryset(self):
+		return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
 
 def vote(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
